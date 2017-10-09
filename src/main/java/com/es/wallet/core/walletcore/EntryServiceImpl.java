cbaee85db94288df74c5178aef759b2043faa5b3
Ehtesham Siddiqui <ehtesham.siddiqui@teradata.com>
2017-10-09 19:40:26 -0400

    changes to repository

--- src/main/java/com/es/wallet/core/walletcore/EntryServiceImpl.java
+++ src/main/java/com/es/wallet/core/walletcore/EntryServiceImpl.java
@@ -23,7 +23,7 @@ public class EntryServiceImpl implements EntryService {
     public List<Entry> findAll() {
         long count = this.repo.count();
         System.out.println("the total number of rows in entry table are: " + count);
-        //this.repo.save(new Entry(2,"INFO","salman","salman","sallyboy","polyboy.com","sally@gmail.com","sall&","samepwd",""));
+         //this.repo.save(new Entry(5,"INFO","fatima","fatima's sample link","fem-d","fem.d.com","fem-d@gmail.com","femm&","samepwd","9008"));
          count = this.repo.count();
         System.out.println("the total number of rows in entry table are: " + count);
         //return new ArrayList<Entry>();
